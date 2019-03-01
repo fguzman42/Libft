dfbbac753c1118cca225cc9afbbcb5bb675fbd02
Francisco Guzman <fguzman@e1z3r1p7.42.us.org>
2019-03-01 13:00:33 -0800

    added new functions, strchr, strcchr and working on strstr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,69 @@
 /*   By: fguzman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 21:36:04 by fguzman           #+#    #+#             */
-/*   Updated: 2019/02/27 22:13:08 by fguzman          ###   ########.fr       */
+/*   Updated: 2019/02/27 23:30:44 by fguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <unistd.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int i;
+	int len;
 
-	i = 0;
-	while (s[ft_strlen(s)] > 0)
+	len = ft_strlen(s);
+	while (len >= 0)
 	{
-		if (s[ft_strlen(s)] == c)
-		return ((char*)s);
-		s--;
+		if (s[len] == c)
+			return (char *)&s[len];
+		len--;
 	}
-	if (*s == c)
-		return ((char *)s);
 	return (NULL);
 }
+/*
+char		*ft_strrchr(const char *s, int c)
+{
+	char ch = c;
+	int i = 0;
+	const char *ptr = s;
+	int	len = ft_strlen(s);
+	while (*s)
+		s++;
+	while (len > 0)
+	{
+		if (*s == ch)
+			return (char *)&ptr[len];
+		len--;
+		s--;
+	}
+	return (NULL);
+}
+
+
+int		main(void)
+{
+	int len;
+	const char str[] = "http//www.tutorialspoint.com";
+	const char ch = '0';
+	char *ret;
+
+	ret = ft_strrchr(str, ch);
+	printf("%s\n", ret);
+
+	char *str2 = "leo is fucking awesome and you know it 0.-";
+	int len2 = ft_strlen(str2) - 1;
+	printf("%s  %c \n", str2, str2[len2]);
+	str2++;
+	printf("%s %c \n",str2,  str2[len2-1]);
+	str2--;
+	printf("%s %c \n", str2, str2[len2]);
+	int i = 0;
+	while (i++ < 10)
+		str2++;
+	printf("%s\n", str2);
+	
+	return 0;
+}
+*/
--- ft_strstr.c
+++ ft_strstr.c
@@ -1,26 +1,25 @@
-/* ************************************************************************ */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fguzman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 15:40:08 by fguzman           #+#    #+#             */
-/*   Updated: 2019/02/27 22:19:54 by fguzman          ###   ########.fr       */
+/*   Created: 2019/02/27 23:37:29 by fguzman           #+#    #+#             */
+/*   Updated: 2019/03/01 12:59:35 by fguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
 
-int	main(void)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int len;
-	const char str[] = "http//www.tutorialspoint.com";
-	const char ch = '.';
-	char *ret;
 
-	ret = ft_strrchr(str, ch);
-	printf("string after |%c|is - |%s|\n", ch, ret);
-	return 0;
+	i = ft_strchr
+
+	while (*haystack)
+	{
+		if (*needle == haystack[i]
+
+	}
 }
