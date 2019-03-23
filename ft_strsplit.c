0bfec8bcccf04931beb3413972d209b6e2cff7a4
Francisco Guzman <fguzman@e1z3r1p17.42.us.org>
2019-03-22 18:41:30 -0700

    empty test

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: fguzman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/09 17:45:02 by fguzman           #+#    #+#             */
-/*   Updated: 2019/03/22 18:39:54 by fguzman          ###   ########.fr       */
+/*   Updated: 2019/03/22 18:41:11 by fguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,3 +77,5 @@ char			**ft_strsplit(char const *s, char c)
 	return (str);
 }
 */
+int main(void)
+{}
